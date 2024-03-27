e52a2331e5620aeef7d4e5d025acc27e96593772
Monish Nule <115737071+imonish8@users.noreply.github.com>
2024-03-27 14:34:13 +0530

    Update redis.ts

--- app/redis.ts
+++ app/redis.ts
@@ -5,7 +5,7 @@ if (!process.env.UPSTASH_REDIS_REST_TOKEN) {
 }
 
 const redis = new Redis({
-  url: "https://global-apt-bear-30602.upstash.io",
+  url: "https://settling-egret-46310.upstash.io",
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
 
